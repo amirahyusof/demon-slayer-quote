99cee7277952e340b28bd71098de3c80cc778fa1
Amirah Yusof <mirez.data@gmail.com>
2025-07-28 16:16:48 +0800

    commit data of quotes

---
+++ lib/quotes.ts
@@ -0,0 +1,29 @@
+// data/quotes.ts
+export type Quote = {
+  text: string;
+  author: string;
+};
+
+export const quotes: Quote[] = [
+  { text: 'Set your heart ablaze.', author: 'Rengoku Kyojuro' },
+  { text: 'Don’t stop. Run. Run. Run!', author: 'Zenitsu Agatsuma' },
+  { text: 'No matter how weak or unworthy you feel, keep living.', author: 'Tanjiro Kamado' },
+  { text: 'Feel the rage. The powerful, pure rage of not being able to forgive will become your strength.', author: 'Giyu Tomioka' },
+  { text: 'To protect those who are precious to me, I’ll make my body into a shield.', author: 'Tanjiro Kamado' },
+  { text: 'The bond between Nezuko and me can’t be severed by anyone!', author: 'Tanjiro Kamado' },
+  { text: 'The strong should aid and protect the weak. That’s the duty of the powerful.', author: 'Kyojuro Rengoku' },
+  { text: 'Those who do not wield a blade cannot know the pain of the ones who do.', author: 'Shinobu Kocho' },
+  { text: 'Don’t give up. Even if it’s painful, even if it’s agonizing, don’t give up.', author: 'Tanjiro Kamado' },
+  { text: 'A lesson without pain is meaningless.', author: 'Kyojuro Rengoku' },
+  { text: 'I am a Hashira. I protect humanity with my blade.', author: 'Giyu Tomioka' },
+  { text: 'Stop acting weak. You are capable of more than you think.', author: 'Zenitsu Agatsuma' },
+  { text: 'The flames of determination will never be extinguished.', author: 'Kyojuro Rengoku' },
+  { text: 'A lesson learned through pain is worth a thousand lectures.', author: 'Kyojuro Rengoku' },
+  { text: 'Those who regret their words and deeds are the ones who truly know the meaning of being human.', author: 'Tanjiro Kamado' },
+  { text: 'You must master a single thing. That is your only hope.', author: 'Sakonji Urokodaki' },
+  { text: 'I’ll become stronger, no matter how many times I fall.', author: 'Tanjiro Kamado' },
+  { text: 'Even if I am afraid, I will still move forward.', author: 'Zenitsu Agatsuma' },
+  { text: 'When you lose your way, remember why you started.', author: 'Kyojuro Rengoku' },
+  { text: 'Life is fleeting, but the bonds we forge will remain.', author: 'Kyojuro Rengoku' },
+  // ... We can add 50+ more quotes if you'd like.
+];
