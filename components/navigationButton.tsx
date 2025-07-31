df73d50241ce32729184ada2bba4e42c669ff093
Amirah Yusof <mirez.data@gmail.com>
2025-08-01 00:49:47 +0800

    commit navigation button

---
+++ components/navigationButton.tsx
@@ -0,0 +1,28 @@
+import { Dices, Heart, Music } from "lucide-react"
+import { Button } from "./ui/button"
+
+export default function NavigationButton(){
+
+  return(
+    <div className="flex flex-col items-center justify-center mt-10 text-red-500">
+      <div className="flex flex-row gap-4 mb-4">
+        <Button className="border-2 hover:bg-white cursor-pointer">
+          <Dices />
+          <span className="text-xl">Random Quote</span>
+        </Button>
+        <Button className="border-2 hover:bg-white cursor-pointer">
+          <Heart />
+          <span className="text-xl">Favourite</span>
+        </Button>
+      </div>
+
+      <div>
+        <Button className="border-2 hover:bg-white cursor-pointer">
+          <Music />
+          <span className="text-xl">Playlist</span>
+        </Button>
+      </div>
+      
+    </div>
+  )
+}
