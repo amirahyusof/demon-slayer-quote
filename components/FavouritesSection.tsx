43db38c8c4520d0512f2e4af316901d88fc639d8
Amirah Yusof <mirez.data@gmail.com>
2025-08-19 12:03:11 +0800

    modify layout and fix the layout

--- components/FavouritesSection.tsx
+++ components/FavouritesSection.tsx
@@ -1,18 +1,24 @@
 import { Quote } from "../lib/quotes"
+import { Card } from "./ui/card"
 
 export default function FavoritesSection({ favorites }: { favorites: Quote[] }) {
   return (
     <div>
-      <h2 className="text-white text-3xl font-bold mb-4">Favourite Quotes</h2>
-      <div className="space-y-4">
+      <h1 
+        className="text-5xl text-black sm:text-6xl font-bold"
+        style={{ fontFamily: "Bloodcrow, sans-serif"}}
+      >
+        Favourite Quotes
+      </h1>
+      <div className="space-y-4 flex flex-col justify-center items-center">
         {favorites.length === 0 ? (
           <p className="text-white italic">No favorites yet.</p>
         ) : (
           favorites.map(q => (
-            <div key={q.id} className="bg-zinc-900 p-4 rounded-md border-l-4 border-red-600">
-              <p>{q.text}</p>
-              <p className="text-right text-sm">{q.character}</p>
-            </div>
+            <Card key={q.id} className="shadow-2xl shadow-red-600 border-red-600 mt-2 p-2 md:-ml-20 w-[90%] md:w-[500px] backdrop-blur-sm bg-white/30 rounded-lg">
+              <p className="text-xl italic font-serif">"{q.text}"</p>
+              <p className="text-md -mt-4">{q.character}</p>
+            </Card>
           ))
         )}
       </div>
